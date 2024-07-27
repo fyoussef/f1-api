b95c498bab7a58654736c118ee40b3369148638e
fyoussef <fillipyuou@gmail.com>
2024-07-27 14:13:13 -0300

    doc: add swagger to doc api routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import { TeamsRouter } from "./presentation/routes/teams";
 import { DriversRouter } from "./presentation/routes/drivers";
 import { GrandPrixRouter } from "./presentation/routes/grand-prix";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 
@@ -10,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", TeamsRouter, DriversRouter, GrandPrixRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => `✔ Server is running!`);
