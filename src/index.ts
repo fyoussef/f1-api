38d3988c67f30b424af1957b60ef6e5162c36730
fyoussef <fillipyuou@gmail.com>
2024-08-05 19:20:21 -0300

    refactor: add middleware to routes that don't exist

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,19 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 
 const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", TeamsRouter, DriversRouter, GrandPrixRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use((req, res) => {
+  res.status(404);
+  res.json({
+    error: "Not Found",
+    message: `To see available routes, access: ${req.protocol}://${req.hostname == "localhost" ? req.hostname + ":" + PORT : req.hostname}/api-docs`,
+  });
+});
 
 app.listen(3000, () => console.log(`✔ Server is running!`));
